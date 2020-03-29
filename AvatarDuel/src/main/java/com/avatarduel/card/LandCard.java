5bb95a602ae61624ed866d41217727fe4387ced6
Yonatan <48679462+haverzard@users.noreply.github.com>
2020-03-29 21:26:17 +0700

    Fixing Card package

--- AvatarDuel/src/main/java/com/avatarduel/card/LandCard.java
+++ AvatarDuel/src/main/java/com/avatarduel/card/LandCard.java
@@ -1,4 +1,13 @@
 package com.avatarduel.card;
 
-public class LandCard {
+import com.avatarduel.element.Element;
+
+public class LandCard extends Card {
+    public LandCard(String _name, String _desc, Element _element, String _imgUrl) {
+        super(_name,_desc,_element, _imgUrl);
+    }
+
+    public Card clone() {
+        return new LandCard(getName(), getDesc(), getElement(), getImgUrl());
+    }
 }
--- AvatarDuel/src/main/java/com/avatarduel/card/SkillCard.java
+++ AvatarDuel/src/main/java/com/avatarduel/card/SkillCard.java
@@ -1,7 +1,18 @@
 package com.avatarduel.card;
 
+import com.avatarduel.element.Element;
+
 public class SkillCard extends Card implements HasCostAttribute {
     private int cost;
+
+    public SkillCard(String _name, String _desc, Element _element, String _imgUrl, int _cost) {
+        super(_name,_desc,_element, _imgUrl);
+        cost = _cost;
+    }
+
+    public Card clone() {
+        return new SkillCard(getName(), getDesc(), getElement(), getImgUrl(), cost);
+    }
 
     // Getter & Setter
     public int getCost() {
