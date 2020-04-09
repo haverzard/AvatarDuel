10be920f0df5570b721bff3d721bc8bc89419c59
mdaffad <13518141@std.stei.itb.ac.id>
2020-04-09 21:02:19 +0700

    add baseline for another test

---
+++ AvatarDuel/src/test/java/testCard.java
@@ -0,0 +1,29 @@
+import com.avatarduel.card.*;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+public class testCard {
+
+    @Test
+    public  void testLandGameCard() {
+
+    }
+    @Test
+    public void testCharacterGameCard() {
+
+    }
+
+    @Test
+    public  void testAuraSkillGameCard() {
+
+    }
+
+    @Test
+    public void testDestroySkillGameCard() {
+
+    }
+
+    @Test
+    public void testPowerUpSkillGameCard() {
+
+    }
+}
